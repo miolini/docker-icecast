633b2cb3fe4e0d365c00fa3dcb540cc66de19984
root <root@broadcast.c.golangshow.internal>
2015-08-31 16:46:13 +0000

    clean start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-#Xvfb -screen 0 1024x768x16 -ac >> /root/xvfb.log 2>> /root/xvfb.log &
-#x11vnc -storepasswd skype /tmp/vncpass
-#pause 5
-#x11vnc -rfbauth /tmp/vncpass -display :0 -forever >> /root/x11vnc.log 2>> /root/x11vnc.log &
-#pulseaudio --system >> /root/pulseaudio.log 2>> /root/pulseaudio.log &
-#export DISPLAY=:0
-#wmaker >> /root/wmaker.log 2>> /root/wmaker.log &
-
 sudo -u icecast2 icecast2 -c /etc/icecast2/icecast.xml >> /root/icecast2.log 2>> /root/icecast2.log &
-
-#darkice -c /etc/darkice.cfg >> /root/darkice.log 2>> /root/darkice.log &
-
 nginx
-
-#echo Start Skype
-#skype >> /root/skype.log 2>> /root/skype.log &
 bash
